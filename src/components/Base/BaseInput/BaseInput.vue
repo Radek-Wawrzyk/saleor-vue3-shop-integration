c98f9169fff5625992fb9ff1a45a85019b89e9d2
Radziu <radekwawrzyk@gmail.com>
2021-10-29 00:22:12 +0100

    Base - BaseInput styles & logic

---
+++ src/components/Base/BaseInput/BaseInput.vue
@@ -0,0 +1,103 @@
+<template>
+  <div
+    class="base-input"
+    :class="[
+      isFocused || modelValue ? 'base-input--active' : false,
+      error ? 'base-input--error' : false,
+      disabled ? 'base-input--disabled': false,
+    ]"
+  >
+    <label
+      class="base-input__label"
+      :class="[
+        isFocused || modelValue ? 'base-input__label--is-focus' : false,
+      ]"
+      :for="id"
+      v-if="label"
+    >
+      {{ label }}
+    </label>
+
+    <input
+      :value="modelValue"
+      :type="type"
+      :id="id"
+      :name="name"
+      :class="[
+        !!error ? 'base-input__inner--error' : false,
+      ]"
+      :title="name"
+      :aria-label="name"
+      @input="$emit('update:modelValue', $event.target.value)"
+      @focus="isFocused = true; $emit('focus')"
+      @focusout="isFocused = false"
+      class="base-input__inner"
+    />
+
+    <transition name="fade">
+      <div
+        class="base-input__error"
+        aria-label="error"
+        v-if="error"
+      >
+        {{ error }}
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue';
+import { uuid } from '@/helpers/index';
+
+export default defineComponent({
+  name: 'BaseInput',
+  emits: ['focus', 'update:modelValue'],
+  props: {
+    error: {
+      type: String as PropType<string>,
+      required: false,
+      default: () => (null),
+    },
+    label: {
+      type: String as PropType<string>,
+      required: false,
+      default: () => (null),
+    },
+    id: {
+      type: String as PropType<string>,
+      required: false,
+      default: () => (uuid()),
+    },
+    name: {
+      type: String as PropType<string>,
+      required: false,
+      default: () => (''),
+    },
+    modelValue: {
+      type: [String, Number] as PropType<string|number>,
+      required: false,
+      default: () => (''),
+    },
+    type: {
+      type: String as PropType<string>,
+      required: false,
+      default: () => ('text'),
+    },
+    disabled: {
+      type: Boolean as PropType<boolean>,
+      required: false,
+      default: () => (false),
+    },
+  },
+  setup() {
+    const isFocused = ref(false);
+
+    return {
+      isFocused,
+    };
+  },
+});
+</script>
+
+<style lang="scss" src="./BaseInput.scss" />
--- src/views/StyleGuide/StyleGuide.vue
+++ src/views/StyleGuide/StyleGuide.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="style-guide">
-    <div class="container">
-      <section class="style-guide__section">
+    <section class="style-guide__section style-guide__section--accent">
+      <div class="container">
         <h2 class="style-guide__section-heading">
           Buttons
         </h2>
@@ -33,23 +33,67 @@
             Primary Button
           </BaseButton>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
+
+    <section class="style-guide__section">
+      <div class="container">
+        <h2 class="style-guide__section-heading">
+          Inputs
+        </h2>
+
+        <div class="style-guide__content style-guide__content--flex-grid">
+          <base-input
+            v-model="name"
+            name="name"
+            label="Name"
+          />
+
+          <base-input
+            v-model="fullname"
+            name="name"
+            label="Error"
+            :error="fullname.length <= 0 ? 'The field is required' : null"
+          />
+
+          <base-input
+            disabled
+            name="Disabled"
+            label="Disabled"
+          />
+        </div>
+
+        <div class="style-guide__content style-guide__content--flex-grid">
+          <span>
+            {{ name }}
+          </span>
+
+          <span>
+            {{ fullname }}
+          </span>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
 import BaseButton from '@/components/Base/BaseButton/BaseButton.vue';
+import BaseInput from '@/components/Base/BaseInput/BaseInput.vue';
 
 export default {
   name: 'StyleGuide',
-  components: { BaseButton },
+  components: { BaseButton, BaseInput },
   setup() {
     const isButtonLoading = ref(false);
+    const name = ref('');
+    const fullname = ref('');
 
     return {
       isButtonLoading,
+      name,
+      fullname,
     };
   },
 };
