ee4b14e3d2028666f75e9b48d48d3cdde8b9d9df
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-11-01 12:32:58 +0100

    #7 main-navigation: create main-navigation components + small config

---
+++ src/components/Main/MainNavigation/MainNavigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="main-navigation">
+    <ul class="list">
+      <li class="list-item" v-for="link in megaMenu" :key="link.id" @mouseover="showSubcategories">
+        {{ link.name }}
+
+        <ul class="list-subcategory">
+          <li
+            class="list-subcategory__item"
+            v-for="category in link.subcategory"
+            :key="category.id"
+          >
+            <h3 class="list-subcategory__item-title">{{ category.name }}</h3>
+            <ul>
+              <li class="list-subcategory__item-sub" v-for="item in category.list" :key="item">
+                {{ item.name }}
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, onMounted } from 'vue';
+import { megaMenu } from './megamenu';
+
+export default defineComponent({
+  setup() {
+    const showSubcategories = () => {
+      console.log('Menu was hovered');
+    };
+    onMounted(() => {
+      console.log(megaMenu);
+    });
+    return { megaMenu, showSubcategories, onMounted };
+  },
+});
+</script>
+
+<style lang="scss" src="./MainNavigation.scss" />
---
+++ src/components/Main/MainNavigation/megamenu.js
@@ -0,0 +1,74 @@
+export const megaMenu = [
+  {
+    id: 1,
+    name: 'Nowości',
+    subcategory: [
+      {
+        id: 1,
+        name: 'Polecane',
+        list: [
+          { name: 'Member days', path: '/member-day' },
+          { name: 'Przeglądaj wszystkie nowości', path: '/' },
+          { name: 'Bestseellery', path: '/bestseeller' },
+          { name: 'Kalendarz premier', path: '/premieres-calendar' },
+        ],
+      },
+      {
+        id: 2,
+        name: 'Nowości dla mężczyzn',
+        list: [
+          { name: 'Buty', path: '/shoes' },
+          { name: 'Ubrania', path: '/cloths' },
+          { name: 'Sprzęt', path: '/equipment' },
+          { name: 'Przeglądaj wszystkie nowości dla mężczyzn', path: '/new-man' },
+        ],
+      },
+      {
+        id: 3,
+        name: 'Nowości dla kobiet',
+        list: [
+          { name: 'Buty', path: '/shoes' },
+          { name: 'Ubrania', path: '/cloths' },
+          { name: 'Sprzęt', path: '/equipment' },
+          { name: 'Przeglądaj wszystkie nowości dla kobiet', path: '/new-woman' },
+        ],
+      },
+      {
+        id: 4,
+        name: 'Nowości dla dzieci',
+        list: [
+          { name: 'Buty', path: '/shoes' },
+          { name: 'Ubrania', path: '/cloths' },
+          { name: 'Sprzęt', path: '/equipment' },
+          { name: 'Przeglądaj wszystkie nowości dla kobiet', path: '/new-woman' },
+        ],
+      },
+      {
+        id: 5,
+        name: 'Nowości dla kobiet',
+        list: [
+          { name: 'Buty', path: '/shoes' },
+          { name: 'Ubrania', path: '/cloths' },
+          { name: 'Sprzęt', path: '/equipment' },
+          { name: 'Przeglądaj wszystkie nowości dla kobiet', path: '/new-woman' },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Mężczyźni',
+  },
+  {
+    id: 3,
+    name: 'Kobiety',
+  },
+  {
+    id: 4,
+    name: 'Dzieci',
+  },
+  {
+    id: 5,
+    name: 'Wyprzedaż',
+  },
+];
